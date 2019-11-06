6546241fb1389e7beceee803fc1c789e3508afac
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-11-06 19:22:38 +0100

    Initial Commit

---
+++ PaperBoxTests/PaperBoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  PaperBoxTests.swift
+//  PaperBoxTests
+//
+//  Created by Mathis Detourbet on 6/11/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import XCTest
+@testable import PaperBox
+
+class PaperBoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
